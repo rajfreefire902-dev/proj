9c0b3059d6301e4442f44dd9b569fce5b9841581
rajfreefire902-dev <rajfreefire902@gmail.com>
2026-04-03 22:07:23 +0530

    Delete server directory

--- server/routes/chatRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const auth = require("../middleware/authMiddleware"); // Import fix
-const { chat } = require("../controllers/chatController");
-
-router.post("/", auth, chat);
-
-module.exports = router;
--- server/routes/compareRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const upload = require("../middleware/uploadMiddleware");
-const auth = require("../middleware/authMiddleware"); // Added
-const { compare } = require("../controllers/compareController");
-
-router.post("/", auth, upload.fields([{ name: "file1" }, { name: "file2" }]), compare);
-
-module.exports = router;
