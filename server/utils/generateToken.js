db8a438eb963ae059493c8ecb66460c6ac109a36
rajfreefire902-dev <rajfreefire902@gmail.com>
2026-04-03 21:54:43 +0530

    Delete server directory

--- server/utils/generateToken.js
+++
@@ -1,8 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-// this will create token
-const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET);
-};
-
-module.exports = generateToken;
